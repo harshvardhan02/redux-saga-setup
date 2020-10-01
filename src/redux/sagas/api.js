d87bf17bc81ba8bc92c71e698681c5d0e018d203
harshvardhan <harshvardhan.singh1991@gmail.com>
2020-10-01 16:44:22 +0530

    change in api file

--- src/redux/sagas/api.js
+++ src/redux/sagas/api.js
@@ -1,5 +1,6 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/users';
-const apiLoginUrl = 'https://reqres.in/api/login'
+const apiLoginUrl = 'https://reqres.in/api/login';
+const apiGetUser = 'https://reqres.in/api/users';
 
 export function getApi() {
     return fetch(apiUrl, {
@@ -23,6 +24,21 @@ export function postApi(credentials) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(credentials)
+    })
+        .then(res => {
+            return res.json()
+        })
+        .then(payload => {
+            return payload
+        });
+}
+
+export function getUsers() {
+    return fetch(apiGetUser, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
         .then(res => {
             return res.json()
